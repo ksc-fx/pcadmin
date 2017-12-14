cd5406faf236f958ec5aa4cc24acef5430cf2fae
hupengfei <hupengfei@kingsoft.com>
2017-12-14 09:56:19 +0800

    Update:eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,12 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
+  extends: ["eslint:recommended"],
+  plugins: ["prettier", "import"],
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // 4空格缩进
-    'indent': ['warn', 4],
-    // 语句强制分号结尾
-    'semi': ['warn', 'always', { 'omitLastInOneLineBlock': true }],
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "prettier/prettier": "warn",
+    "prefer-arrow-callback": "warn",
   }
 };
